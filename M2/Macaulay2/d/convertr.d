848ba4128e7aecf1429e29fe0f4e41b4c34adcbf
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-08-26 02:13:28 +0200

    fixed the order of FilePosition components

--- M2/Macaulay2/d/convertr.d
+++ M2/Macaulay2/d/convertr.d
@@ -21,10 +21,21 @@ export UnaryInstallMethodFun := dummyTernaryFun;
 export InstallValueFun := dummyMultaryFun;
 export UnaryInstallValueFun := dummyTernaryFun;
 
-combinePosition(a:Position,b:Position):Position := Position(a.filename,b.line,b.column,a.line1,a.column1,b.line2,b.column2,a.loadDepth); -- combine with emphasis on right
-combinePositionLeft(a:Position,b:Position):Position := Position(a.filename,a.line,a.column,a.line1,a.column1,b.line2,b.column2,a.loadDepth); -- combine with emphasis on left
-combinePosition(a:Position,b:Position,c:Position):Position := Position(a.filename,c.line,c.column,a.line1,a.column1,b.line2,b.column2,a.loadDepth); -- left, right, center
-combinePositionAdjacent(a:Position,b:Position):Position := Position(a.filename,a.line2,a.column2,a.line1,a.column1,b.line2,b.column2,a.loadDepth); -- special for Adjacent
+-- combine two positions with emphasis borrowed from beginning of the first one
+combinePositionL(L:Position, R:Position):Position := Position(
+    L.filename, L.lineL, L.columnL, R.lineR, R.columnR, L.lineF, L.columnF, L.loadDepth);
+
+-- combine two positions with emphasis borrowed from endpoint of the second one
+combinePositionR(L:Position, R:Position):Position := Position(
+    L.filename, L.lineL, L.columnL, R.lineR, R.columnR, R.lineF, R.columnF, L.loadDepth);
+
+-- combine two positions with emphasis borrowed from a third, central point
+combinePositionC(L:Position, R:Position, C:Position):Position := Position(
+    L.filename, L.lineL, L.columnL, R.lineR, R.columnR, C.lineF, C.columnF, L.loadDepth);
+
+-- combine two positions belonging to adjacent tokens (focus is on endpoint of the first one)
+combinePositionAdjacent(L:Position, R:Position):Position := Position(
+    L.filename, L.lineL, L.columnL, R.lineR, R.columnR, L.lineR, L.columnR, L.loadDepth);
 
 convert0(e:ParseTree):Code;
 convert(e:ParseTree):Code;
